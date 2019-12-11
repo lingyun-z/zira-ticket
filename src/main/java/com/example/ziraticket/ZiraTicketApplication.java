f61991e87e92183feb01528a4609a043d043af44
Zhang Lingyun <zhang.lingyun@pwc.com>
2019-12-11 22:01:31 +0800

    init commit

---
+++ src/main/java/com/example/ziraticket/ZiraTicketApplication.java
@@ -0,0 +1,13 @@
+package com.example.ziraticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZiraTicketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZiraTicketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ziraticket/ZiraTicketApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.ziraticket;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZiraTicketApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
